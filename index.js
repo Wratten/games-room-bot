b974aa034d332e65290b777a5eeeb25b50338e0e
Wratten <daniel.wratten@gmail.com>
2022-10-08 13:43:26 +0800

    randomised bot responses to rock paper scissors game and added comments

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const { Client, GatewayIntentBits, userMention, User } = require('discord.js');
+const { Client, GatewayIntentBits, userMention, User, MessageFlags } = require('discord.js');
 const dotenv = require('dotenv')
 dotenv.config()
 
 // getting access to servers, their messages and their message contents for use later
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
+const rpsArr = ["rock", "paper", "scissors"];
+
+// function rpsRand(rpsArr) {
+//     const randomIndex = Math.floor(Math.random() * rpsArr.length);
+
+//     const item = rpsArr[randomIndex];
+
+//     return item;
+// 
 
 
 client.on('ready', () => {
@@ -12,14 +21,17 @@ client.on('ready', () => {
 })
 
 client.on('messageCreate', (message) => {
+    
+    const randomMessage = rpsArr[Math.floor(Math.random() * rpsArr.length)];
+
     if (message.content === 'rock' && !message.author.bot) {
-        message.reply('paper')
+        message.reply(randomMessage);
     }
     if (message.content === 'paper' && !message.author.bot) {
-        message.reply('scissors')
+        message.reply(randomMessage);
     }
     if (message.content === 'scissors' && !message.author.bot) {
-        message.reply('rock')
+        message.reply(randomMessage);
     }
 })
 
